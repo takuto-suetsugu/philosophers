f6f8187814dba5cd232302c4a8a91ded8e5cc546
Takuto Suetsugu <tsuetsug@c1r21s5.42tokyo.jp>
2022-03-25 17:39:33 +0900

    些細な変更

--- philo/srcs/philo_act.c
+++ philo/srcs/philo_act.c
@@ -6,7 +6,7 @@
 /*   By: tsuetsug <tsuetsug@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 15:41:29 by tsuetsug          #+#    #+#             */
-/*   Updated: 2022/03/25 17:37:03 by tsuetsug         ###   ########.fr       */
+/*   Updated: 2022/03/25 17:37:49 by tsuetsug         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void*    philo_act(void *argv)
 {
 	t_data	*data;
-	
+
 	data = argv;
 	if (data->philo_id % 2 == 1 && data->act.sleeping == 0)
 	{	
